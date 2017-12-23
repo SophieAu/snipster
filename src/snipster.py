2b93d4e4d32d09ace79c46b48e372ffe12c086ca
Sophie Au <sophieau@users.noreply.github.com>
2017-12-23 17:25:42 +0100

    Reads command line arguments

--- src/snipster.py
+++ src/snipster.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
 sourceDir = str(os.path.expanduser("~/.snipster"))
-
 print(sourceDir)
+
+def parseCLIArgs(cliArgs):
+    print(cliArgs)
+
+parseCLIArgs(sys.argv)
+
+
+
