473cc6668df679fc342360eeb4704e4b00d71fe2
Sophie Au <sophieau@users.noreply.github.com>
2017-12-23 19:24:07 +0100

    Adds snippet attributes

--- src/Snippet.py
+++ src/Snippet.py
@@ -2,11 +2,23 @@
 
 class Snippet:
     path = ""
+    id = 0
+    tags = []
+    lang = ""
+    title = ""
+    description = ""
+    code = ""
 
     def __init__(self, file, path):
         self.parse(file)
         self.path = path
         print(self.path)
+        print(self.id)
+        print(str(self.tags))
+        print(self.lang)
+        print(self.title)
+        print(self.description)
+        print(self.code)
 
     def parse(self, file):
         if file.readline() != "---\n":
