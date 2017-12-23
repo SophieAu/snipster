ec5980eba12578a6f552e6549055f064810c9750
Sophie Au <sophieau@users.noreply.github.com>
2017-12-23 17:50:45 +0100

    Opens file named in cli argument

--- src/snipster.py
+++ src/snipster.py
@@ -10,6 +10,14 @@ def parseCLIArgs(cliArgs):
     del cliArgs[0] # deletes the snipster command
     print(cliArgs)
 
+    # open file specified in the now 1st argument
+    try:
+        with open(sourceDir + "/" + cliArgs[0]) as snippet:
+            print(snippet.readline()) # print first line of the file
+    except FileNotFoundError:
+        print("File not found")
+
+
 parseCLIArgs(sys.argv)
 
 
