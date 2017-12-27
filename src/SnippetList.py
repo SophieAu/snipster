13fd4ce93dee603fb6b3b4b9589c7e4eb4c5f041
Sophie Au <sophieau@users.noreply.github.com>
2017-12-27 20:14:19 +0100

    Opens a csv snippet list and reads it

--- src/SnippetList.py
+++ src/SnippetList.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-snippetList = []
+import os
 
+snippetList = ""
+sourceDir = str(os.path.expanduser("~/.snipster"))
+snippetListFile = "__snipster__.csv"
 
 
 def lookupSnippetPath(id):
     print("looking up")
+    openSnippetList()
     findSnippet(id)
     return("test.txt")
 
@@ -15,6 +19,16 @@ def showSnippetList(filters):
 
 def sourceSnippets():
     print("Sourcing snippets")
+
+def openSnippetList():
+    print("opening file")
+    try:
+        with open(sourceDir + "/" + snippetListFile) as snippetCSV:
+            snippetList = csv.reader(snippetCSV)
+    except FileNotFoundError:
+        print("Didn't find a snippet list file. You can create one by using the source command ('snipster source')")
+        exit(1)
+
 
 
 def findSnippet(id):
