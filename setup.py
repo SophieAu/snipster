826bd5e0435912a9cd319ee9ad0b70b570ad381e
Sophie Au <sophieau@users.noreply.github.com>
2017-12-30 18:05:47 +0100

    Bump version number to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name         = 'snipster-py',
-    version      = '1.0',
+    version      = '1.0.1',
     author       = 'Sophie Au',
     author_email = 'some.person@web.de',
     license      = 'MIT',
