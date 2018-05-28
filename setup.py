d2ebaa1bf867bec4e2921a8522f2a1425186b749
Sophie Au <sophie.au@user.github.noreply.com>
2018-05-28 16:09:34 +0200

    Adds readme to description in pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name         = 'snipster-py',
     version      = '1.0.3',
@@ -7,6 +10,8 @@ setup(
     author_email = 'some.person@web.de',
     license      = 'MIT',
     description  = 'A simple command line snippet manager',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     keywords     = ['snippet', 'snippets-manager', 'command-line-tool'],
     url          = 'https://github.com/sophieau/snipster',
 
